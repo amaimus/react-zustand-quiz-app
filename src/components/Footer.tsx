4601646f789cb6ae47dca71c170e49f267e12ee9
alejandro <alejandromaimus147@gmail.com>
2023-09-10 17:35:47 +0200

    using hooks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,7 @@
-import { useQuestionsStore } from '../store/questions'
-
+import { useQuestionsData } from '../hook/useQuestionsData'
 
 export function Footer () {
-
-  const questions = useQuestionsStore(state => state.questions)
-
-
-  let correct = 0
-  let incorrect = 0
-  let unanswered = 0
-  
-  questions.forEach(question => {
-    const { userSelectedAnswer, correctAnswer } = question
-    if ( userSelectedAnswer == null) unanswered++
-    else if (userSelectedAnswer === correctAnswer) correct++
-    else incorrect++
-  })
+  const { correct, incorrect, unanswered } = useQuestionsData()
   
   return (
     <footer style={{marginTop: '16px'}}>
