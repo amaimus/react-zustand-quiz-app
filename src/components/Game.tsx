567df97389ce27a026af425f4d5f7ef1079a6a0e
alejandro <alejandromaimus147@gmail.com>
2023-09-10 15:22:21 +0200

    List question answersand styles

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,4 +1,4 @@
-import { IconButton, Stack, Card, Typography} from '@mui/material'
+import { IconButton, Stack, List, Card, Typography, ListItem, ListItemButton, ListItemText} from '@mui/material'
 import { useQuestionsStore } from '../store/questions'
 import { type Question as QuestionType } from '../types'
 import SyntaxHighLighter from 'react-syntax-highlighter'
@@ -6,7 +6,7 @@ import { gradientDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
 function Question ({info} : {info: QuestionType}) {
   return (
-    <Card variant='outlined' sx={{textAlign: 'left', padding: 2}}>
+    <Card variant='outlined' sx={{textAlign: 'left', padding: 2, marginTop: 4}}>
       <Typography variant='h5'>
         {info.question}
       </Typography>
@@ -16,6 +16,18 @@ function Question ({info} : {info: QuestionType}) {
       >
         {info.code}
       </SyntaxHighLighter>
+
+      <List sx={{bgcolor: '#e5e5e5'}} disablePadding>
+        {
+          info.answers.map((answer, index) => (
+            <ListItem key={index} disablePadding divider>
+              <ListItemButton>
+                <ListItemText primary={answer} sx={{textAlign: 'center'}}/>
+              </ListItemButton>
+            </ListItem>
+          ))
+        }
+      </List>
     </Card>
   )
 }
