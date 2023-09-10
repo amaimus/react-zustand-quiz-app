0701b8de29217440744c424f3923a56ac9e98423
alejandro <alejandromaimus147@gmail.com>
2023-09-10 14:24:48 +0200

    Start component and main dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { Container, Stack, Typography } from '@mui/material';
 import './App.css';
+import { JavascriptLogo } from './components/JavascriptLogo';
+import { Start } from './components/Start';
 
 function App() {
 
   return (
-    <>
-      <h1>Quiz app</h1>
-    </>
+    <Container maxWidth='sm'>
+      <Stack direction='row' gap='2' alignItems='center' justifyContent='center'>
+        <JavascriptLogo />
+        <Typography variant='h2' component='h1'>
+          Quiz app
+        </Typography>
+        <Start />
+      </Stack>
+    </Container>
   )
 }
 
---
+++ src/components/Start.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material';
+
+export function Start () {
+  return (
+    <Button
+      onClick={ () => {}}
+      variant='contained'
+    >
+      Start!
+    </Button>
+  )
+
+}
